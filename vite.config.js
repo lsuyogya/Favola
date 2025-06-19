9130e4783ad65992bb2346cd7de590b143b99605
Suyogya Luitel <lsuyogya@gmail.com>
2025-06-19 10:26:04 +0545

    init homepage

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    {
+      name: 'remove-crossorigin',
+      transformIndexHtml(html) {
+        return html.replace(/crossorigin/g, '');
+      },
+    },
+  ],
+  root: '.',
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    open: '/index.html',
+    watch: {
+      usePolling: true,
+    },
+  },
+});
